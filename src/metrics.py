66461c6383985b22d6745e267d1f6d66fe51dfca
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-08-30 09:35:01 +0200

    feat: export e_total data

--- src/metrics.py
+++ src/metrics.py
@@ -27,6 +27,9 @@ FIELDS: set[str] = {
     "e_day_exp",
     "e_day_impl",
     "e_load_day",
+    "e_total",
+    "e_total_exp",
+    "e_total_imp",
     "meter_e_total_exp",
     "meter_e_total_imp",
 }
